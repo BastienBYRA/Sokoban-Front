58944a5f3a97b8c0f4388a1e1336c04f54cc3253
BYRA Bastien <bbyra@u-bordeaux.fr>
2023-03-13 17:18:13 +0100

    ecran homepage avec fake data

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './screens/Home';
+import Board from './screens/Board';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,11 @@ const App = () => {
           name="Home"
           component={Home}
           options={{title: 'Home'}}
+        />
+        <Stack.Screen
+          name="Board"
+          component={Board}
+          options={{title: 'Board'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function Home() {
+export default function Home({ navigation }) {
+    const [board, setBoard] = useState(null);
+
+    useEffect(() => {
+        getAllBoards();
+    }, []);
+
+    getAllBoards = async () => {
+        var json = JSON.parse('[{"id": 1,"name": "Simple board"}, {"id": 2,"name": "Simple board 2"}]')
+        // const response = await fetch('http://127.0.0.1:36987/board');
+        setBoard(json);
+    }
+
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        {board != null &&
+            <FlatList
+                data={board}
+                renderItem={({ item }) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            margin: 1
+                        }}>
+                        <TouchableOpacity
+                        onPress={() => navigation.navigate('Board', {id: item.id})}
+                        >
+                            <Text>
+                                {item.name}
+                            </Text>
+                        </TouchableOpacity>
+
+                    </View>
+                )}
+                //Setting the number of column
+                numColumns={3}
+                keyExtractor={(item, index) => index.toString()}
+                />
+        }
     </View>
   );
 }
