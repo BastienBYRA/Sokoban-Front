ea92f2f1c71ba7dc1f4a542bbfb7588c7c5dbd8f
OButtner <olivier.buttner@gmail.com>
2023-03-13 15:54:00 +0100

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const HomeScreen = () => {
+
+
+    return (
+        <View>
+            <Text>Choisissez votre niveau</Text>
+        </View>
+    )
+
+
+
+};
+
+export default HomeScreen;
